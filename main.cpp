6219e7238996f628c6c231cf3be42c55fdd4a5d9
Yash Krishan Verma <yash.krishan.verma@gmail.com>
2018-04-04 15:03:03 +0530

    Build stairs

--- main.cpp
+++ main.cpp
@@ -1,129 +1,288 @@
 #include <iostream>
 #include <stdlib.h>
-
-#ifdef __APPLE__
-#include <OpenGL/OpenGL.h>
-#include <GLUT/glut.h>
-#else
 #include <GL/glut.h>
-#endif
 
 using namespace std;
 
-float _angle = 30.0f;
-float _cameraAngle = 10.0f;
+float angle_tip = 0.0f, angle_view = 0.0f, angle_tilt = 0.0f;
 
-void handleKeypress(unsigned char key,
-                    int x, int y){
-                        switch(key) {
-                            case 27:
-                                exit(0);
-                        }
-                    }
 
-void initRendering(){
-    //Makes 3D drawing work when something is in front of something else
-    glEnable(GL_DEPTH_TEST);
-    //Setting background color
-    glClearColor(0.7f, 0.9f, 1.0f, 1.0f);
+//Called when a key is pressed
+void handleKeypress(unsigned char key, int x, int y) {
+	switch (key) {
+		case 27: //Escape key
+			exit(0);
+		case 'z':
+			angle_tilt -= 5;
+			break;
+		case 'x':
+			angle_tilt += 5;
+			break;
+	}
+
+	glutPostRedisplay();
 }
 
-void handleResize(int w, int h){
-    //Tell OpenGL how to convert from coordinates to pixel values
-    glViewport(0, 0, w, h);
+void handleSpecialKeypress(int key, int x, int y){
+	switch(key){
+       case GLUT_KEY_LEFT :
+       angle_tip -= 5;  break;
+       case GLUT_KEY_RIGHT:  
+       angle_tip += 5;  break;
+       case GLUT_KEY_UP   :  
+       angle_view -= 5;  break;
+       case GLUT_KEY_DOWN :  
+       angle_view += 5;  break;
+	}
 
-    glMatrixMode(GL_PROJECTION); //Camera prospective
-
-    //Set camera prospective
-    glLoadIdentity();
-    gluPerspective(45.0,                    //Camera angle
-                    (double)w / (double)h,  //Width to height ratio
-                    1.0,                    //Near z clipping coordinate
-                    200.0);                 //Far z clipping coordinate
+	glutPostRedisplay();
 }
 
-void update(int value) {
-    _angle += 2.0f;
-    if (_angle > 360) {
-        _angle -= 360;
-    }
-    
-    glutPostRedisplay(); //Tell GLUT that the scene has changed
-    
-    //Tell GLUT to call update again in 25 milliseconds
-    glutTimerFunc(25, update, 0);
+//Initializes 3D rendering
+void initRendering() {
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_COLOR_MATERIAL);
+	glEnable(GL_LIGHTING); //Enable lighting
+	glEnable(GL_LIGHT0); //Enable light #0
+	glEnable(GL_LIGHT1); //Enable light #1
+	glEnable(GL_NORMALIZE); //Automatically normalize normals
+	//glShadeModel(GL_SMOOTH); //Enable smooth shading
 }
 
-//Draws the 3D scene
-void drawScene_() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
-    glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
-    glLoadIdentity(); //Reset the drawing perspective
-    glRotatef(-_cameraAngle, 0.0f, 1.0f, 0.0f); //Rotate the camera
-    glTranslatef(0.0f, 0.0f, -5.0f); //Move forward 5 units
-    
-    glPushMatrix(); //Save the transformations performed thus far
-    glTranslatef(0.0f, -1.0f, 0.0f); //Move to the center of the trapezoid
-    glRotatef(_angle, 0.0f, 0.0f, 1.0f); //Rotate about the z-axis
-    
-    glBegin(GL_QUADS);
-    
-    //Trapezoid
-    glColor3f(0.5f, 0.0f, 0.8f);
-    glVertex3f(-0.7f, -0.5f, 0.0f);
-    glColor3f(0.0f, 0.9f, 0.0f);
-    glVertex3f(0.7f, -0.5f, 0.0f);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex3f(0.4f, 0.5f, 0.0f);
-    glColor3f(0.0f, 0.65f, 0.65f);
-    glVertex3f(-0.4f, 0.5f, 0.0f);
-    
-    glEnd();
-    
-    glPopMatrix();
-    glPushMatrix();
-    glTranslatef(1.0f, 1.0f, 0.0f);
-    glRotatef(_angle, 0.0f, 1.0f, 0.0f);
-    glScalef(0.7f, 0.7f, 0.7f);
-    
-    glBegin(GL_TRIANGLES);
-    glColor3f(0.0f, 0.75f, 0.0f);
-    
-    //Pentagon
-    glVertex3f(-0.5f, -0.5f, 0.0f);
-    glVertex3f(0.5f, -0.5f, 0.0f);
-    glVertex3f(-0.5f, 0.0f, 0.0f);
-    
-    glVertex3f(-0.5f, 0.0f, 0.0f);
-    glVertex3f(0.5f, -0.5f, 0.0f);
-    glVertex3f(0.5f, 0.0f, 0.0f);
-    
-    glVertex3f(-0.5f, 0.0f, 0.0f);
-    glVertex3f(0.5f, 0.0f, 0.0f);
-    glVertex3f(0.0f, 0.5f, 0.0f);
-    
-    glEnd();
-    
-    glPopMatrix();
-    glPushMatrix();
-    glTranslatef(-1.0f, 1.0f, 0.0f);
-    glRotatef(_angle, 1.0f, 2.0f, 3.0f);
-    
-    glBegin(GL_TRIANGLES);
-    
-    //Triangle
-    glColor3f(1.0f, 0.7f, 0.0f);
-    glVertex3f(0.5f, -0.5f, 0.0f);
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glVertex3f(0.0f, 0.5f, 0.0f);
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex3f(-0.5f, -0.5f, 0.0f);
-    
-    glEnd();
-    
-    glPopMatrix(); //Undo the move to the center of the triangle
-    glutSwapBuffers(); //Send the 3D scene to the screen
+//Called when the window is resized
+void handleResize(int w, int h) {
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, (double)w / (double)h, 1.0, 200.0);
+}
+
+void drawLeftStairSet(){
+
+	//Left Stair Set
+	glNormal3f(-1.0f, 0.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, 0.90f, 3.0f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, 0.90f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, 1.0f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, 1.0f, 3.0f);
+
+	//Left Stair Set
+	glNormal3f(-1.0f, 0.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, 0.25f, 2.05f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, 0.25f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, 0.60f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, 0.60f, 2.05f);
+
+
+	//Left Stair Set
+	glNormal3f(-1.0f, 0.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, -0.40f, 2.05f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, -0.40f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, -0.05f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, -0.05f, 2.05f);
+
+
+	//Left Stair Set
+	glNormal3f(-1.0f, 0.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, -0.05f, 3.0f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, -0.05f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, 0.25f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, 0.25f, 3.0f);
+
+	//Left Stair Set
+	glNormal3f(-1.0f, 0.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, -0.05f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, -0.40f, 2.05f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, -0.05f, 2.05f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, 0.25f, 2.65f);
+}
+
+void drawRightStairSet(){
+
+	//Right Stair Set
+	glNormal3f(1.0f, 0.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, 0.90f, 3.0f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, 0.90f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, 1.0f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, 1.0f, 3.0f);
+
+	//Right Stair Set
+	glNormal3f(1.0f, 0.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, 0.25f, 2.05f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, 0.25f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, 0.60f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, 0.60f, 2.05f);
+
+
+	//Right Stair Set
+	glNormal3f(1.0f, 0.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, -0.05f, 3.0f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, -0.05f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, 0.25f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, 0.25f, 3.0f);
+
+
+	//Right Stair Set
+	glNormal3f(1.0f, 0.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, -0.05f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, 0.25f, 2.05f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, 0.60f, 2.05f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, 0.25f, 2.65f);
+
+
+	//Right Stair Set
+	glNormal3f(1.0f, 0.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, -0.40f, 3.0f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, -0.40f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, -1.0f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, -1.0f, 3.0f);
+
+
+	//Right Stair Set
+	glNormal3f(1.0f, 0.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, -0.40f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, -0.70f, 1.75f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, -1.0f, 1.75f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, -1.0f, 2.65f);
+
+
+	//Right Stair Set
+	glNormal3f(1.0f, 0.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, -0.70f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, -0.70f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, -1.0f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, -1.0f, 2.65f);
+
+}
+
+void drawBottomSet(){
+	
+	//Bottom
+	glNormal3f(0.0f, -1.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-1.5f, -1.0f, -1.5f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-1.5f, -1.0f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(1.5f, -1.0f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(1.5f, -1.0f, -1.5f);
+
+	//Bottom
+	glNormal3f(0.0f, -1.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, -1.0f, -1.5f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, -1.0f, 3.0f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, -1.0f, 3.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, -1.0f, -1.5f);
+
+
+	//Bottom
+	glNormal3f(0.0f, -1.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, -0.40f, 2.05f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, -0.05f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, -0.05f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.0f, -0.40f, 2.05f);
+
+
+	//Bottom
+	glNormal3f(0.0f, -1.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, -0.05f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, -0.05f, 3.0f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, -0.05f, 3.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, -0.05f, 2.65f);
+
+	//Bottom
+	glNormal3f(0.0f, -1.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.0f, 0.25f, 2.05f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, -0.05f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, -0.05f, 2.65f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, 0.25f, 2.05f);
+
+	//Bottom
+	glNormal3f(0.0f, -1.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, 0.25f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, 0.25f, 2.05f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, 0.25f, 2.05f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, 0.25f, 1.5f);
+
+	//Bottom
+	glNormal3f(0.0f, -1.0f, 0.0f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-0.25f, -0.40f, 1.5f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, -0.40f, 2.05f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, -0.40f, 2.05f);
+	//glNormal3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(0.25f, -0.40f, 1.5f);
 
 }
 
@@ -142,21 +301,38 @@ void drawScene() {
 	
 	//Add positioned light
 	GLfloat lightColor0[] = {0.5f, 0.5f, 0.5f, 1.0f}; //Color (0.5, 0.5, 0.5)
-	GLfloat lightPos0[] = {4.0f, 0.0f, 8.0f, 1.0f}; //Positioned at (4, 0, 8)
+	GLfloat lightPos0[] = {100.0f, 100.0f, 100.0f, 1.0f}; //Positioned at (4, 0, 8)
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
 	glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
 	
 	//Add directed light
-	GLfloat lightColor1[] = {0.5f, 0.2f, 0.2f, 1.0f}; //Color (0.5, 0.2, 0.2)
+	GLfloat lightColor1[] = {0.0f, 0.2f, 0.2f, 1.0f}; //Color (0.5, 0.2, 0.2)
 	//Coming from the direction (-1, 0.5, 0.5)
 	GLfloat lightPos1[] = {-1.0f, 0.5f, 0.5f, 0.0f};
 	glLightfv(GL_LIGHT1, GL_DIFFUSE, lightColor1);
 	glLightfv(GL_LIGHT1, GL_POSITION, lightPos1);
 	
-	glRotatef(_angle, 0.0f, 1.0f, 0.0f);
+	glRotatef(angle_tip, 0.0f, 1.0f, 0.0f);
+	glRotatef(angle_view, 1.0f, 0.0f, 0.0f);
+	glRotatef(angle_tilt, 0.0f, 0.0f, 1.0f);
 	glColor3f(1.0f, 1.0f, 0.0f);
 	glBegin(GL_QUADS);
+
+	//Front Stair Wall
+	glNormal3f(0.0f, 0.0f, 1.0f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, -1.0f, 3.0f);
+	//glNormal3f(1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, -1.0f, 3.0f);
+	//glNormal3f(1.0f, 0.0f, 1.0f);
+	glVertex3f(0.25f, 1.0f, 3.0f);
+	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.25f, 1.0f, 3.0f);
 	
+	drawLeftStairSet();
+
+	drawRightStairSet();	
+
 	//Front
 	glNormal3f(0.0f, 0.0f, 1.0f);
 	//glNormal3f(-1.0f, 0.0f, 1.0f);
@@ -200,28 +376,50 @@ void drawScene() {
 	glVertex3f(-1.5f, 1.0f, 1.5f);
 	//glNormal3f(-1.0f, 0.0f, -1.0f);
 	glVertex3f(-1.5f, 1.0f, -1.5f);
-	
+
+	drawBottomSet();
+
 	glEnd();
 	
 	glutSwapBuffers();
 }
 
-int main(int argc, char** argv){
-
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(400, 400);
-
-    glutCreateWindow("Project");
-    initRendering();
-
-    glutDisplayFunc(drawScene);
-    glutKeyboardFunc(handleKeypress);
-    glutReshapeFunc(handleResize);
-    glutTimerFunc(25, update, 0); //Add a timer
-
-    glutMainLoop();
-    return 0;
-
+void update(int value) {
+	angle_tip = value;
+	angle_tip += 1.5f;
+	if (angle_tip > 360) {
+		angle_tip -= 360;
+	}
+	
+	glutPostRedisplay();
 }
+
+int main(int argc, char** argv) {
+	//Initialize GLUT
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(400, 400);
+	
+	//Create the window
+	glutCreateWindow("Project");
+	initRendering();
+	
+	//Set handler functions
+	glutDisplayFunc(drawScene);
+	glutKeyboardFunc(handleKeypress);
+	glutSpecialFunc(handleSpecialKeypress);
+	glutReshapeFunc(handleResize);
+	
+	//glutTimerFunc(25, update, 0); //Add a timer
+	
+	glutMainLoop();
+	return 0;
+}
+
+
+
+
+
+
+
 
